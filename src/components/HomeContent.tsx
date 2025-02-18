9369989e634a374fbbeb01ebaf41cdaa8497cf3b
Daryna Bondarenko <daryna.bondarenko@osf.digital>
2025-02-18 20:48:05 +0100

    - Created list post - Added TW - Updated package.json - Added TW configs

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -1,103 +1,55 @@
 "use client";
 
-import styles from "@/app/page.module.css";
-import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import Link from "next/link";
 
-function HomeContent({ data }) {
-  console.log("data");
-  console.log(data);
+type TPostProps = {
+  id: string;
+  title: string;
+  body: string;
+};
+
+function HomeContent({ id, title, body }: TPostProps) {
+  const textRef = useRef<HTMLParagraphElement>(null);
+  const [shouldClamp, setShouldClamp] = useState(false);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const lineHeight = parseFloat(
+        getComputedStyle(textRef.current).lineHeight,
+      );
+      const maxHeight = lineHeight * 3; // Maximum height for 3 lines
+
+      if (textRef.current.scrollHeight > maxHeight) {
+        setShouldClamp(true);
+      }
+    }
+  }, [body]);
+
   return (
-    <div className={styles.page}>
-      {/*{data.map((item) => (*/}
-      {/*  <div>{item.title}</div>*/}
-      {/*))}*/}
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <section
+      key={id}
+      className="mx-auto flex flex-col justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    >
+      <section className="flex flex-col">
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h2>
+        <p
+          ref={textRef}
+          className={`font-normal text-gray-700 dark:text-gray-400 ${shouldClamp ? "line-clamp-3" : ""}`}
+        >
+          {body}
+        </p>
+      </section>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Link
+        href="#"
+        className="mt-5 px-3 max-w-[100px] py-2 text-sm font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-600"
+      >
+        Read more
+      </Link>
+    </section>
   );
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,10 @@
 import axiosInstance from "@/lib/axios";
 
-export const getPosts = async () => {
-  return await axiosInstance.get("https://jsonplaceholder.typicode.com/posts");
+export const getPosts = async ({ maxPosts }) => {
+  const response = await axiosInstance.get(
+    "https://jsonplaceholder.typicode.com/posts",
+  );
+  const posts = response.data;
+
+  return posts.slice(0, maxPosts);
 };
