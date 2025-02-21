db4511f66a3db11c6d3a387fd47af4f27b59b64a
danitas <danitas.bonitas@gmail.com>
2025-02-21 21:03:18 +0100

    - Added loader

--- src/components/HomeDetail/index.tsx
+++ src/components/HomeDetail/index.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { usePostStore } from "@/context/PostStoreContext";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Loader from "@/components/Loader";
 import EditModal from "@/components/EditModal";
 
@@ -12,11 +12,11 @@ type Props = {
 export const HomeDetail = ({ id }: Props) => {
   const { setPosts, posts } = usePostStore();
 
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleStateModal = () => {
-        setIsOpen((prevState) => !prevState);
-    };
+  const handleStateModal = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
   useEffect(() => {
     setPosts([]);
@@ -36,15 +36,15 @@ export const HomeDetail = ({ id }: Props) => {
       <p className="mt-5 mb-6 text-lg font-normal text-gray-500 lg:text-xl">
         {post?.body}
       </p>
-        <EditModal {...post} isOpen={isOpen} close={handleStateModal}>
-            <button
-                type="button"
-                onClick={handleStateModal}
-                className="cursor-pointer w-[110px] md:w-[150px] mb-5 px-3 py-2 text-sm md:text-lg font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-600"
-            >
-                Edit Post
-            </button>
-        </EditModal>
+      <EditModal {...post} isOpen={isOpen} close={handleStateModal}>
+        <button
+          type="button"
+          onClick={handleStateModal}
+          className="cursor-pointer w-[110px] md:w-[150px] mb-5 px-3 py-2 text-sm md:text-lg font-medium text-center text-white bg-lime-700 rounded-lg hover:bg-lime-600"
+        >
+          Edit Post
+        </button>
+      </EditModal>
     </section>
   );
 };
